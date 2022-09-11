9dbfbc8faa6c89d20db901993bb1cc1647220949
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-09-11 17:07:46 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import axios from 'axios';
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
