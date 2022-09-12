f18fb8598acd925a0082b5594018bb303f7f8a76
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-09-12 14:45:46 +0300

    Add refs, markup, key, url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+import axios from 'axios';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import axios from 'axios';
 // import SimpleLightbox from "simplelightbox";
 // import "simplelightbox/dist/simple-lightbox.min.css";
+import getRefs from './js/getRefs';
+
+const KEY = '29871741-92f0be7d75d630b941b41d19d';
+const BASIC_URL = `https://pixabay.com/api/?key=${KEY}&q=`;
+
+
+const refs = getRefs();
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.galleryEl.addEventListener('click', onGalleryImgClick);
+refs.loadBtn.addEventListener('click', onLoadBtnClick);
+
+
+refs.loadBtn.disabled = true;
+
+let page = 1;
+let searchQuery = '';
+
+
+ 
---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+    return {
+        formEl: document.querySelector('#search-form'),
+        loadBtn: document.querySelector('.load-more'),
+        galleryEl: document.querySelector('.gallery'),
+    };
+}
