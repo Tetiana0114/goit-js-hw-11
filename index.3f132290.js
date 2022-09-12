25bfc5bcef1df6ae035f227fba7ffb79cafdb2d1
Tetiana0114 <100161722+Tetiana0114@users.noreply.github.com>
2022-09-12 17:20:23 +0000

    Deploying to gh-pages from @ Tetiana0114/goit-js-hw-11@508329a043c346b02eb219003395c8944e2123fb 🚀

---
+++ index.3f132290.js
@@ -0,0 +1,2 @@
+!function(){var e={formEl:document.querySelector("#search-form"),loadBtn:document.querySelector(".load-more"),galleryEl:document.querySelector(".gallery")};e.formEl.addEventListener("submit",onFormSubmit),e.galleryEl.addEventListener("click",onGalleryImgClick),e.loadBtn.addEventListener("click",onLoadBtnClick)}();
+//# sourceMappingURL=index.3f132290.js.map
