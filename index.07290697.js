c5bd7b9792c5a70b151d909111bfaf9f10996745
Tetiana0114 <100161722+Tetiana0114@users.noreply.github.com>
2022-09-14 15:01:34 +0000

    Deploying to gh-pages from @ Tetiana0114/goit-js-hw-11@53d625cd6c73c0b849dd3549fbc2adc86a4c2098 🚀

--- index.07290697.js
+++
@@ -1,2 +0,0 @@
-const e={formEl:document.querySelector("#search-form"),loadBtn:document.querySelector(".load-more"),galleryEl:document.querySelector(".gallery")};e.formEl.addEventListener("submit",onFormSubmit),e.galleryEl.addEventListener("click",onGalleryImgClick),e.loadBtn.addEventListener("click",onLoadBtnClick);
-//# sourceMappingURL=index.07290697.js.map
