6cab7a7f426ab7fd0df3f259678bdc912d68bd6b
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-09-17 00:15:38 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -2,32 +2,27 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 import { makeGalleryPhotoCardMarkup } from './js/createPhotoCardMarkup';
 import { smoothPageScrolling } from './js/smoothPageScrolling';
 import getRefs from './js/getRefs';
+import { BASIC_URL, searchFields } from './js/fetchGallery';
 
-const KEY = '29871741-92f0be7d75d630b941b41d19d';
-const BASIC_URL = `https://pixabay.com/api/?key=${KEY}&q=`;
-const searchFields = `&image_type=photo&orientation=horizontal&safesearch=true`;
 let page = 1;
 let perPage = 40;
 
 async function fetchGallery(request) {
   const response = await axios.get(`${BASIC_URL}${request}${searchFields}&page=${page}&per_page=${perPage}`);
-  const searchInfo = await response.data;
   if (request.trim() === '') {
     return;
   }
-  return searchInfo;
+  return await response.data;
 }
-
-const refs = getRefs();
 
 let lightboxGallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 let searchQuery = '';
+const refs = getRefs();
 
 updateLoadBtnClassList('is-hidden', 'is-visible');
 
@@ -40,7 +35,7 @@ function resetGallery() {
 
 function buildGalleryOnPage(e) {
   const galleryPhoto = makeGalleryPhotoCardMarkup(e);
-  refs.galleryEl.insertAdjacentHTML('beforeend', galleryPhoto)
+  refs.galleryEl.insertAdjacentHTML('beforeend', galleryPhoto);
 }
 
 function onFormSubmit(event) {
@@ -50,6 +45,7 @@ function onFormSubmit(event) {
       resetGallery();
       updateLoadBtnClassList('is-hidden', 'is-visible');
   }
+  resetGallery();
   getNotificationOnPage(searchQuery);
 }
 
@@ -77,6 +73,7 @@ async function getNotificationOnPage(searchQuery) {
       updateLoadBtnClassList('is-visible', 'is-hidden');
     } else {
       Notify.failure('Sorry, there are no images matching your search query. Please, try again.');
+      updateLoadBtnClassList('is-hidden', 'is-visible');
     }
     lightboxGallery.refresh();
     page += 1;
--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,2 +1,3 @@
-
-
+export const KEY = '29871741-92f0be7d75d630b941b41d19d';
+export const BASIC_URL = `https://pixabay.com/api/?key=${KEY}&q=`;
+export const searchFields = `&image_type=photo&orientation=horizontal&safesearch=true`;
